bc3d8abc83e3b5cd5c7e0a4e24f7cf1b11f90684
sscruz <s.scruz.312@gmail.com>
2017-01-16 16:22:38 +0100

    Full implementation of kmeans with weights per event

---
+++ kMeansWeights.C
@@ -0,0 +1,174 @@
+#include "kMeansWeights.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "TRandom3.h"
+#include "TMath.h"
+#include "TGraph.h"
+
+using namespace std;
+
+kMeansWeights::kMeansWeights(Int_t k)
+{
+  fK = k;
+  readFromFiles();
+
+}
+
+void kMeansWeights::readFromFiles()
+{
+  
+  fN = 0;
+  ifstream f; f.open("data/ttbartest.txt");
+  while (f){
+    Double_t x = 0; Double_t y = 0; Double_t w = 0;
+    f >> x >> y >> w;
+    fX.push_back(x);
+    fY.push_back(y);
+    fW.push_back(w);
+    fN ++;
+  }
+  f.close();
+
+
+}
+
+// Double_t kMeansWeights::getMax( Double_t* data, Double_t coeff)
+// {
+//   Double_t max = -9999;
+//   for (int n = 0; n < fN; ++n){
+//     if ( coeff*data[n] > max)
+//       max = data[n];
+//   }
+//   if ( max == -9999) cout << "[E]: Maximum hasnt been calculated" << endl;
+//   return max;
+// }
+
+Double_t kMeansWeights::d(Double_t x, Double_t y, Double_t m_x, Double_t m_y) 
+{
+  return TMath::Sqrt( (x-m_x) * (x-m_x) + (y-m_y) * (y-m_y) );
+}
+
+
+void kMeansWeights::MakeAssignment()
+{
+  // Assigns each data point to its cluster :)
+  for (int n = 0; n < fN; ++n){
+    Int_t cluster = -1;
+    Double_t dist = 10000;
+    for (int k = 0; k < fK; ++k){
+      Double_t dst = d( fX[n], fY[n], fMx[k], fMy[k]);
+      //      cout << "A" << fX[k] << " " <<  fY[k] << " " <<  fMx[k] << " " <<  fMy[k] << endl;
+      // cout << "B" << k << " " << dist<< " " << dst << endl;
+      if (dst < dist){
+	cluster = k;
+	dist = dst;
+      }
+    }
+    if (cluster < 0){ cout << "[E]: Nearest cluster not found" << endl;}
+    fTgt[n] = cluster; 
+  }
+}
+
+Int_t kMeansWeights::CalculateCentroids()
+{
+  Int_t theyChange = 1;
+  Double_t* fMWght = new Double_t[fK];
+  Double_t* fOldMx = new Double_t[fK];
+  Double_t* fOldMy = new Double_t[fK];
+
+  for (int k = 0; k < fK; ++k){
+    fOldMx[k] = fMx[k];
+    fOldMy[k] = fMy[k];
+    fMx[k] = 0; fMy[k] = 0;
+    fMWght[k] = 0;
+  }
+
+  for (int n = 0; n < fN; ++n){
+    fMx[fTgt[n]]    +=  fW[n] * fX[n];
+    fMy[fTgt[n]]    +=  fW[n] * fY[n];
+    fMWght[fTgt[n]] +=  fW[n];
+  }
+
+  Double_t di = 0;
+  
+  for (int k = 0; k < fK; ++k){
+    fMx[k] /= fMWght[k];
+    fMy[k] /= fMWght[k];
+    di += d(fMx[k],fMy[k], fOldMx[k], fOldMy[k]);
+  }
+  if (di < 1.0e-8) theyChange =  -1;
+  cout << "[I]: New centroids are (" << fMx[0] << "," << fMy[0]
+       << ") and (" << fMx[1] << "," << fMy[1] << ")" << endl;
+
+  delete fMWght;
+  delete fOldMx;
+  delete fOldMy;
+
+  return theyChange;
+
+}
+
+void kMeansWeights::Init()
+{
+  fMx  = new Double_t[fK];
+  fMy  = new Double_t[fK];
+  // fX   = new Double_t[fN];
+  // fY   = new Double_t[fN];
+  // fW   = new Double_t[fN];
+  fTgt = new Int_t[fN];
+
+}
+
+void kMeansWeights::fit()
+{
+  TRandom3* r = new TRandom3();
+  Double_t maxX = (*max_element(fX.begin(), fX.end()));
+  Double_t minX = (*min_element(fX.begin(), fX.end()));
+  Double_t maxY = (*max_element(fY.begin(), fY.end()));
+  Double_t minY = (*min_element(fY.begin(), fY.end()));
+
+  Init();
+
+  // Random inizialitation of centroids
+  for (int k = 0; k < fK; ++k){
+    fMx[k] = r->Uniform(minX, maxX);
+    fMy[k] = r->Uniform(minY, maxY);
+  }
+  // Cluster assignation
+  Int_t maxIt = 999999;
+  Int_t it = 0;
+
+  while (it < maxIt){
+    it++;
+    MakeAssignment();
+    if (CalculateCentroids() < 0) break;
+
+  }
+  if ( maxIt == it) cout << "[W]: Maximum number of iterations reached!! ";
+  
+  fDataX = new vector<Double_t>[fK];
+  fDataY = new vector<Double_t>[fK];
+  for (int n = 0; n < fN; ++n){
+    fDataX[fTgt[n]].push_back( fX[n] );
+    fDataY[fTgt[n]].push_back( fY[n] );
+  }
+
+  delete r;
+
+}
+
+void kMeansWeights::ScatterPlot()
+{
+
+  TGraph* gr1 = new TGraph( fDataX[0].size(), &fDataX[0][0], &fDataY[0][0]);
+  TGraph* gr2 = new TGraph( fDataX[1].size(), &fDataX[1][0], &fDataY[1][0]);
+  gr1->SetMarkerColor(kBlue);
+  gr2->SetMarkerColor(kRed);
+  gr1->SetMarkerStyle(kFullCircle);
+  gr2->SetMarkerStyle(kFullCircle);
+  
+  gr1->Draw("AP");
+  gr2->Draw("PSAME");
+
+}
---
+++ kMeansWeights.h
@@ -0,0 +1,33 @@
+#ifndef KMEANSWEIGHTS_H
+#define KMEANSWEIGHTS_H
+
+class kMeansWeights
+{
+ public: 
+  kMeansWeights(Int_t k) ;
+  ~kMeansWeights() {}
+  void fit();
+  void ScatterPlot();
+
+ protected:
+  Int_t      fK;     // numer of clusters
+  Double_t*  fMx;    // centroid position X
+  Double_t*  fMy;    // centroid position Y
+  vector<Double_t>  fX;     // data component X
+  vector<Double_t>  fY;     // data component Y
+  vector<Double_t>  fW;     // data weight
+  Int_t*     fTgt;   // target
+  Int_t      fN;     // number of entries
+  vector<Double_t>* fDataX; // data in clusters
+  vector<Double_t>* fDataY; // data in clusters
+
+  void     Init();
+  Double_t d(Double_t, Double_t, Double_t, Double_t);
+  void     MakeAssignment();
+  Int_t    CalculateCentroids();
+  void     readFromFiles();
+
+};
+
+
+#endif

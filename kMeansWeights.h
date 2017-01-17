6a81ff8cc4783f87d9aa25b4621215de2a739e66
sscruz <s.scruz.312@gmail.com>
2017-01-17 10:24:50 +0100

    More stuff that i forgot to add

--- kMeansWeights.h
+++ kMeansWeights.h
@@ -8,7 +8,8 @@ class kMeansWeights
   ~kMeansWeights() {}
   void fit();
   void ScatterPlot();
-
+  void makeHistos();
+  void MCScatterPlots();
  protected:
   Int_t      fK;     // numer of clusters
   Double_t*  fMx;    // centroid position X
@@ -16,17 +17,30 @@ class kMeansWeights
   vector<Double_t>  fX;     // data component X
   vector<Double_t>  fY;     // data component Y
   vector<Double_t>  fW;     // data weight
+
   Int_t*     fTgt;   // target
   Int_t      fN;     // number of entries
   vector<Double_t>* fDataX; // data in clusters
   vector<Double_t>* fDataY; // data in clusters
+
+  // Monte Carlo
+  vector<Double_t>  fTTbarX;     // data component X
+  vector<Double_t>  fTTbarY;     // data component Y
+  vector<Double_t>  fTTbarW;     // data weight
+  vector<Double_t>  fTTHX;     // data component X
+  vector<Double_t>  fTTHY;     // data component Y
+  vector<Double_t>  fTTHW;     // data weight
+  vector<Double_t>  fTTWX;     // data component X
+  vector<Double_t>  fTTWY;     // data component Y
+  vector<Double_t>  fTTWW;     // data weight
 
   void     Init();
   Double_t d(Double_t, Double_t, Double_t, Double_t);
   void     MakeAssignment();
   Int_t    CalculateCentroids();
   void     readFromFiles();
-
+  Int_t    GetCluster(Double_t, Double_t);
+  Int_t    classicalBinning(Double_t, Double_t);
 };
 
 

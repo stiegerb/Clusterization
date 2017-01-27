0836aa92c32642833f86afeb667982280f8f4c1b
Pietro Vischia <pietro.vischia@gmail.com>
2017-01-27 01:03:54 +0100

    Improved output for runTrials (now seed is searchable in batck logs)

--- runTrials.C
+++ runTrials.C
@@ -3,19 +3,21 @@ void runTrials(int trial){
   // Run me with:
   // root -l -b runTrials.C
 
+  cout << "Running trial " << trial << endl;
+
   gROOT->ProcessLine(".L MakeSimpleCard.C+");
   gROOT->ProcessLine(".L Significance.C+");
   gROOT->ProcessLine(".L TargettedClustering.C+");
 
 
-  gROOT->ProcessLine("TargettedClustering* g = new TargettedClustering(2,2,0)");
+  gROOT->ProcessLine("TargettedClustering* g");
   
   //for(int trial=1; trial<1001; ++trial)
   //  {
-      gROOT->ProcessLine("delete g");
-      gROOT->ProcessLine(Form("g = new TargettedClustering(2,2,%d)", trial));
+      gROOT->ProcessLine(Form("g = new TargettedClustering(2,3,%d)", trial));
       gROOT->ProcessLine("g->Test()");
       gROOT->ProcessLine("g->SignificancesEachLevel()");
+      gROOT->ProcessLine("delete g");
       //  }
-  
+      gApplication->Terminate(0);
 }

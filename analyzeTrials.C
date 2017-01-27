d655a2ab98691bb61dba3b2f85456df527fd8485
Pietro Vischia <pietro.vischia@gmail.com>
2017-01-27 01:04:35 +0100

    Bugfix, and display other checks of interest for the trials

--- analyzeTrials.C
+++ analyzeTrials.C
@@ -3,45 +3,76 @@
 #include <TGraph.h>
 #include <TTree.h>
 #include <TCanvas.h>
+#include <TH1.h>
+#include <TH2D.h>
+#include <TStyle.h>
 
 using namespace std;
 
 void analyzeTrials(int nTrials){
 
+  gStyle->SetOptStat(0);
+
   std::vector<TGraph*> g; g.clear();
+  std::vector<TGraph*> gnbins; gnbins.clear();
   TFile* output=nullptr;
+  TFile* shapes=nullptr;
   TTree* Tlimit=nullptr;
+  TH1* nbins=nullptr;
+  TH2D* dep = new TH2D("dep", "Number of trials per bin;Level;Nbins", 7, 0.5, 7.5, 20, 0.5, 20.5);
   double levels[7] = {1., 2., 3., 4., 5., 6., 7.};
 
   double minLimit(99999.);
   double minLabel(-1);
   for(int trial=1; trial<nTrials; ++trial)
     {
+      cout << "[" << trial << "/" << nTrials << "]" << endl;
       double limits[7];
+      double nb[7];
       for(int level=1; level<8; ++level)
         {
           output = TFile::Open(Form("higgsCombineTrial%d.Asymptotic.mH%d.root",trial,level));
-          if(!output) continue;
-          Tlimit = (TTree*) output->Get("limit");
-          if(!Tlimit) continue;
-          Double_t limit(0);
-          Tlimit->SetBranchAddress("limit",&limit);
-          Tlimit->GetEntry(3);
-          limits[level]=limit;
-          cout << "Level: " << level << ", limit: " << limit << endl;
-          if(level==7)
+          shapes = TFile::Open(Form("datacard%d_Level_%d_shapes.root",trial,level));
+          
+          if(output)
             {
-              if(limit<minLimit)
+              Tlimit = (TTree*) output->Get("limit");
+              if(Tlimit)
                 {
-                  minLimit=limit;
-                  minLabel=trial;
+                  Double_t limit(0);
+                  Tlimit->SetBranchAddress("limit",&limit);
+                  Tlimit->GetEntry(2);
+                  limits[level]=limit;
+                  if(level==7)
+                    {
+                      if(limit<minLimit)
+                        {
+                          minLimit=limit;
+                          minLabel=trial;
+                        }
+                    }
+                  delete Tlimit;
+                  output->Close();
+                  delete output;
                 }
             }
-          output->Close();
-          delete output;
+          if(shapes)
+            {
+              nbins = (TH1*) shapes->Get("data_obs");
+              nb[level] = nbins->GetNbinsX();
+              delete nbins;
+              shapes->Close();
+              delete shapes;
+            }
         }
       g.push_back(new TGraph(7, levels, limits));
-      cout << "Fetched value for trial " << trial << endl;
+      g[trial-1]->SetName(Form("g%d",trial));
+      gnbins.push_back(new TGraph(7, levels, nb));
+      gnbins[trial-1]->SetName(Form("nbi%d",trial));
+      for(size_t lev=0; lev<7; ++lev)
+        {
+          dep->Fill(levels[lev], nb[lev]);
+        }
     }
   
   TCanvas* c = new TCanvas("c", Form("%d bootstrap trials", nTrials), 1000, 1000);
@@ -62,8 +93,35 @@ void analyzeTrials(int nTrials){
     }
   c->Modified();
   c->Update();
-  c->Print("bootstrapTrials.png");
-  c->Print("bootstrapTrials.pdf");
+  c->Print("expectedLimitTrials.png");
+  c->Print("expectedLimitTrials.pdf");
+
+  c->Clear();
+  c->Update();
+  painted=false;
+  for(auto& graph : gnbins)
+    {
+      if(!painted)
+        {
+          graph->Draw("APL");
+          graph->GetHistogram()->SetMaximum(20.);
+          painted=true;
+        }
+      else
+        graph->Draw("PL");
+    }
+  c->Modified();
+  c->Update();
+  c->Print("nbinsPerLevelTrials.png");
+  c->Print("nbinsPerLevelTrials.pdf");
+
+  c->Clear();
+  c->Update();
+  dep->Draw("contz");
+  c->Modified();
+  c->Update();
+  c->Print("fulldependency.png");
+  c->Print("fulldependency.pdf");
 
 
   cout << "Minimum is at " << minLimit << ", corresponding to trial " << minLabel << endl;

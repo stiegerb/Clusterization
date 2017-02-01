d11f6b7136d3fe5b722c3e025a310598f10c87bd
Pietro Vischia <pietro.vischia@gmail.com>
2017-02-01 18:18:31 +0100

    Fixed parameter

--- runTrialsBatch.sh
+++ runTrialsBatch.sh
@@ -3,6 +3,6 @@
 NTRIALS=$1
 MODULE=$2
 for trial in $(seq 1 ${NTRIALS}); do
-    qsub trialTemplate.sh -F ${trial} ${MODULE}
+    qsub trialTemplate.sh -F "${trial} ${MODULE}"
     #sh trialTemplate.sh ${trial}
 done
